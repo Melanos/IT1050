8a7db48d089e9662d298a2abf7b6409793399865
Melanos <aikanaro@live.ru>
2017-03-19 20:29:12 -0700

    Hw added

---
+++ LabAssignment3/LabAssignment3/Instructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LabAssignment3
+{
+    class Instructor
+    {
+        private string Name;
+        private string CourseName;
+
+        public Instructor(string name, string coursename)
+        {
+            Name = name;
+            CourseName = coursename;
+        }
+        public void SetStudentGrade(Student student, int grade)
+        {
+            student.SetGrade(grade);
+        }
+
+        public string Course()
+        {
+            return "Instructor: " + Name.PadRight(8) + " Course: " + CourseName;
+        }
+
+        public void PrintCourse()
+        {
+            Console.WriteLine(Course());
+        }
+    }
+}
---
+++ LabAssignment3/LabAssignment3/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LabAssignment3
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher;
+
+        public Student(string name, Instructor teacher)
+        {
+            Name = name;
+            Teacher = teacher;
+            Grade = 0;
+        }
+
+        public void SetGrade(int grade)
+        {
+            Grade = grade;
+        }
+
+        public string StudentGrade()
+        {
+            return "Student: " + Name.PadRight(8) + " Grade: " + Grade;
+        }
+
+        public void PrintGradeTeacher()
+        {
+            Console.WriteLine(StudentGrade() + " ".PadRight(8) + Teacher.Course());
+        }
+    }
+}
