d47f3ee8d77ce7e60409e782b9513cd9db712420
Melanos <aikanaro@live.ru>
2017-05-04 17:37:45 -0700

    Homework

---
+++ Chess solution/Almost Chess2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Almost_Chess2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board ChessBoard = new Board();
+            ChessBoard.ChessBoardPic();
+        }
+    }
+}
